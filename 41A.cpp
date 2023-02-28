98a7b204332e6765f7ad0f94d8c8e6dd82468584
AshrafRahul <m.ashraful.islam007@gmail.com>
2023-02-28 10:46:14 +0600

    problem solving

---
+++ 41A.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s1,s2;
+    cin>>s1;
+    cin>>s2;
+
+    reverse(s1.begin(), s1.end());
+
+    if(s1==s2) cout<<"YES\n";
+    else cout<<"NO\n";
+
+    return 0;
+}
