048e64240fab4e98a90bdc94846f8a5019166374
AshrafRahul <m.ashraful.islam007@gmail.com>
2023-05-19 16:54:18 +0600

    problem solving

---
+++ 141A.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string a,b,c,s;
+    cin>>a>>b>>c;
+
+    s = a+b;
+
+    sort(s.begin(), s.end());
+    sort(c.begin(), c.end());
+
+    if(s==c) cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+
+    return 0;
+}
