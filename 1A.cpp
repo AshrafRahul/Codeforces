b791b0ea30e9637c284f76215f50d0dce7bbabb3
AshrafRahul <m.ashraful.islam007@gmail.com>
2023-05-27 21:52:48 +0600

    problem solving

---
+++ 1A.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    long long m,n,a,x,y;
+    cin>>m>>n>>a;
+
+    if(m%a==0) x=m/a;
+    else x=m/a +1;
+
+    if(n%a==0) y=n/a;
+    else y=n/a +1;
+
+    cout<<x*y<<endl;
+
+    return 0;
+}
