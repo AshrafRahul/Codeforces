e041679f19fc832544c6db3b674b152f9de552ae
Md Ashraful Islam <m.ashraful.islam007@gmail.com>
2024-01-15 23:27:06 +0600

    problem solve

---
+++ 451A.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n,m;
+    cin >> n >> m;
+
+    int k = min(n,m);
+
+    if(k%2==0) cout << "Malvika" <<endl;
+    else cout << "Akshat" << endl;
+
+    return 0;
+}
